c27d0e2def89bf286f90c5a7e85f03f155f6673e
Grady Zhuo <gradyzhuo@gmail.com>
2026-04-10 09:21:10 +0800

    [UPDATE] bump version to 1.0.2

--- Sources/OrbitalCore/Commands/OrbitalCommand.swift
+++ Sources/OrbitalCore/Commands/OrbitalCommand.swift
@@ -4,7 +4,7 @@ public struct OrbitalCommand: ParsableCommand {
     public static let configuration = CommandConfiguration(
         commandName: "orbital",
         abstract: L10n.Orbital.abstract,
-        version: "1.0.1",
+        version: "1.0.2",
         subcommands: [
             SetupCommand.self,
             InitCommand.self,
