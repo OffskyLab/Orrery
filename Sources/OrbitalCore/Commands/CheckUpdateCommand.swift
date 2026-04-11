08bcd516c515bc90a303a4038c617530e99c3315
Grady Zhuo <gradyzhuo@gmail.com>
2026-04-11 20:46:40 +0800

    [FIX] import FoundationNetworking for Linux URLSession support

--- Sources/OrbitalCore/Commands/CheckUpdateCommand.swift
+++ Sources/OrbitalCore/Commands/CheckUpdateCommand.swift
@@ -1,5 +1,8 @@
 import ArgumentParser
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 public struct CheckUpdateCommand: ParsableCommand {
     public static let configuration = CommandConfiguration(
